0092a4ff87c18f121ee9581c553392f0cdf2d6d9
danishz <danishvv1011@gmail.com>
2025-09-28 16:51:53 +0530

    nn

---
+++ index.js
@@ -0,0 +1,28 @@
+import express, { json } from "express";
+import * as dotenv from "dotenv";
+import loginRoute from "./routes/login.js";
+import dbConnection from "./config/dbconfig.js";
+import taskRoute from "./routes/taskRoute.js";
+import profileRoute from "./routes/profileRoute.js";
+import cors from "cors";
+
+const app = express();
+console.log("Hi");
+
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+    res.send("sda");
+});
+
+app.use("/login", loginRoute);
+app.use("/task", taskRoute);
+app.use("/profile", profileRoute);
+
+app.listen(5050, () => {
+    dbConnection();
+    console.log(`working at http://localhost:${process.env.PORT}`);
+});
